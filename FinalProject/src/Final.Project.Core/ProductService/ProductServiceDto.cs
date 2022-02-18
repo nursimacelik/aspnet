b7f4bc5813475822212adaaf2d0ef1c4514f4f32
nursimacelik <nnn3575@gmail.com>
2022-02-18 21:59:30 +0300

    Remove old comments

--- FinalProject/src/Final.Project.Core/ProductService/ProductServiceDto.cs
+++ FinalProject/src/Final.Project.Core/ProductService/ProductServiceDto.cs
@@ -21,30 +21,23 @@ namespace Final.Project.Core.ProductServices
         public bool IsOfferable { get; set; }
         public bool IsSold { get; set; }
     }
+
     public class CreateProductInput
     {
-        //[Required]
-        //[StringLength(100, ErrorMessage = "{0} field max {1} chars")]
         public string ProductName { get; set; }
-        //[Required]
-        //[StringLength(500, ErrorMessage = "{0} field max {1} chars")]
         public string Description { get; set; }
-        //[Required]
         public int CategoryId { get; set; }
         public int ColorId { get; set; }
         public int BrandId { get; set; }
-        //[Required]
         public int UsingStatusId { get; set; }
-        //[Required]
         public string Image { get; set; }
-        //[Required]
         public decimal Price { get; set; }
         public bool IsOfferable { get; set; }
 
     }
+
     public class UpdateProductInput : CreateProductInput
     {
-        //[Required]
         public int Id { get; set; }
         public bool IsSold { get; set; }
     }
